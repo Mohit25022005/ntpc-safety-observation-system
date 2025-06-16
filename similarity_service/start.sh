48edce9cbf07aa585b24c5b3d24b82db7a9f9410
Mohit <mohitswarnkar13@gmail.com>
2025-06-16 10:57:36 +0530

    updates

--- similarity_service/start.sh
+++ similarity_service/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "🔁 Starting Similarity Service on http://localhost:8000 ..."
-uvicorn similarity_service:app --host 0.0.0.0 --port 8000 --reload
+echo "🚀 Starting Similarity Service on Render..."
+gunicorn -k uvicorn.workers.UvicornWorker similarity_service:app --bind 0.0.0.0:$PORT
