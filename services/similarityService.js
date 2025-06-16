1f11cbe7cff9e6e1b919cdacf5600987d472dbf1
Mohit <mohitswarnkar13@gmail.com>
2025-06-16 10:51:59 +0530

    updates

--- services/similarityService.js
+++ services/similarityService.js
@@ -1,9 +1,10 @@
-// services/similarityService.js
 const axios = require('axios');
+
+const SIMILARITY_SERVICE_URL = process.env.SIMILARITY_SERVICE_URL || 'http://localhost:8000';
 
 const getSimilarityScores = async (newText, existingReports) => {
     try {
-        const response = await axios.post('http://localhost:8000/similarity', {
+        const response = await axios.post(`${SIMILARITY_SERVICE_URL}/similarity`, {
             new_text: newText,
             existing: existingReports.map(r => ({
                 id: r._id.toString(),
@@ -11,7 +12,6 @@ const getSimilarityScores = async (newText, existingReports) => {
             }))
         });
 
-        // response.data includes: is_duplicate and similar_matches
         return {
             is_duplicate: response.data.is_duplicate,
             similar_matches: response.data.similar_matches
